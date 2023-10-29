fdff4e85efe4065ab4ba89a11e2b5a4525821396
InesMsallem <98458100+InesMsallem@users.noreply.github.com>
2023-10-29 16:49:17 +0100

    --

--- front/src/app/components/group-list/group-list.component.ts
+++ front/src/app/components/group-list/group-list.component.ts
@@ -7,16 +7,31 @@ import { TutorialService } from 'src/app/services/tutorial.service';
   styleUrls: ['./group-list.component.css']
 })
 export class GroupListComponent implements OnInit {
-  groups: any[] = [];
-  searchTerm: string = '';
-  constructor(private tutorialService: TutorialService) {}
+filteredGroups: any[] = [];
+allGroups: any[] = [];
+searchTerm: string = '';
+groups: any[] = [];
 
-  ngOnInit() {
-    this.getGroups();
-  }
+constructor(private tutorialService: TutorialService) {}
 
-  getGroups() {
-    this.tutorialService.getGroups().subscribe(
+ngOnInit() {
+  this.getGroups();
+}
+
+getGroups() {
+  this.tutorialService.getGroups().subscribe(
+    (data: any) => {
+      this.allGroups = data;
+      this.filteredGroups = this.allGroups; // Initialize filteredGroups
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+searchGroups() {
+  if (this.searchTerm) {
+    this.tutorialService.searchGroups(this.searchTerm).subscribe(
       (data: any) => {
         this.groups = data;
       },
@@ -24,22 +39,32 @@ export class GroupListComponent implements OnInit {
         console.log(error);
       }
     );
+  } else {
+    this.getGroups(); 
   }
+}
 
-  searchGroups() {
-    if (this.searchTerm) {
-      this.tutorialService.searchGroups(this.searchTerm).subscribe(
-        (data: any) => {
-          this.groups = data;
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-    } else {
-      this.getGroups(); 
-    }
+filterByType(type: string) {
+  if (type === 'public') {
+    this.filteredGroups = this.allGroups.filter((group) => group.type === 'http://www.semanticweb.org/inès/ontologies/2023/9/untitled-ontology-2#publicGroup');
+  } else if (type === 'private') {
+    this.filteredGroups = this.allGroups.filter((group) => group.type === 'http://www.semanticweb.org/inès/ontologies/2023/9/untitled-ontology-2#privateGroup');
   }
-  
+}
+
+showAllGroups() {
+  this.filteredGroups = this.allGroups; // Show all groups
+}
+
+getTypeName(type: string): string {
+  // You can implement a method to extract the type name from the URL here
+  if (type.includes('privateGroup')) {
+    return 'Private';
+  } else if (type.includes('publicGroup')) {
+    return 'Public';
+  } else {
+    return 'Unknown';
+  }
+}
 
 }
